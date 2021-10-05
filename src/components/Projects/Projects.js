8506c1a590d8a026b050e4bf228cbab8fce9c65c
Bryan <cortinabryan@gmail.com>
2021-10-04 18:42:26 -0700

    added project components and technologies

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -20,45 +20,35 @@ import {
 } from "../../styles/GlobalComponents";
 import { projects } from "../../constants/constants";
 
-const projectsDemo = [
-  {
-    title: "Project 1",
-    description: "This is a really really long description about this project",
-  },
-  {
-    title: "Project 2",
-    description: "This is a really really long description about this project",
-  },
-  {
-    title: "Project 3",
-    description: "This is a really really long description about this project",
-  },
-  {
-    title: "Project 4",
-    description: "This is a really really long description about this project",
-  },
-  {
-    title: "Project 5",
-    description: "This is a really really long description about this project",
-  },
-  {
-    title: "Project 6",
-    description: "This is a really really long description about this project",
-  },
-];
-
 const Projects = () => (
   <Section nopadding id="projects">
     <SectionDivider />
     <SectionTitle main>Projects</SectionTitle>
     <GridContainer>
-      {projectsDemo.map((project) => (
-        <div>
-          {project.title}
-          <br />
-          {project.description}
-        </div>
-      ))}
+      {projects.map(
+        ({ id, image, title, description, tags, source, visit }) => (
+          <BlogCard key={id}>
+            <Img src={image} />
+            <TitleContent>
+              <HeaderThree title>{title}</HeaderThree>
+              <Hr />
+            </TitleContent>
+            <CardInfo>{description}</CardInfo>
+            <div>
+              <TitleContent>Stack</TitleContent>
+              <TagList>
+                {tags.map((tag, i) => (
+                  <Tag key={i}>{tag}</Tag>
+                ))}
+              </TagList>
+            </div>
+            <UtilityList>
+              <ExternalLinks href={visit}>Code</ExternalLinks>
+              <ExternalLinks href={source}>Source</ExternalLinks>
+            </UtilityList>
+          </BlogCard>
+        )
+      )}
     </GridContainer>
   </Section>
 );
--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,12 +1,51 @@
-import React from 'react';
-import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
-import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
+import React from "react";
+import { DiFirebase, DiReact, DiZend } from "react-icons/di";
+import {
+  Section,
+  SectionDivider,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import {
+  List,
+  ListContainer,
+  ListItem,
+  ListParagraph,
+  ListTitle,
+} from "./TechnologiesStyles";
 
-const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+const Technologies = () => (
+  <Section>
+    <SectionDivider />
+    <SectionTitle>Technologies</SectionTitle>
+
+    <SectionText>
+      I've worked with a range a technologies in the web development world.
+      Mostly Front-End
+    </SectionText>
+    <List>
+      <ListItem>
+        <DiFirebase size="3rem" />
+        <ListContainer>
+          <ListTitle>Front-End</ListTitle>
+        </ListContainer>
+        <ListParagraph>
+          Experience with <br />
+          React.js
+        </ListParagraph>
+      </ListItem>
+      <ListItem>
+        <DiFirebase size="3rem" />
+        <ListContainer>
+          <ListTitle>Back-End</ListTitle>
+        </ListContainer>
+        <ListParagraph>
+          Experience with <br />
+          React.js
+        </ListParagraph>
+      </ListItem>
+    </List>
+  </Section>
 );
 
 export default Technologies;
