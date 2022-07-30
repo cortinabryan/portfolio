8fe005982cf5b7e1e18f34baef67e4b40c5ab1ce
cortinabryan <64225493+cortinabryan@users.noreply.github.com>
2022-07-30 12:02:09 -0700

    updated images dir file

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -3,7 +3,7 @@ export const projects = [
     title: "Martial-Arts Check-in",
     description:
       "Using Vanilla HTML, CSS and Javascript. I decided to create a Martial-arts Check-in App. The concept is similar to a commercial gym check-in app but the main difference is in martial arts we have belts that represents your rank. I've decided it would be a great idea if i can create an app where you can just easily check-in and it also keeps track of your hours so you and your instructors will get notified if when you're due for a promotion",
-    image: "/images/class-sched.png",
+    image: "/public/images/class-sched.PNG",
     tags: ["HTML", "CSS", "Javascript"],
     source: "https://github.com/cortinabryan/Martial-Arts-Check-In-App",
     visit: "https://martial-arts-check-in-app.vercel.app/index.html",
@@ -13,7 +13,7 @@ export const projects = [
     title: "Taz Bulldogs",
     description:
       "Using React, Node.js, Styled Components. I created this dog-breeder website for a coworker of mine. This is my first React Project",
-    image: "/images/1.png",
+    image: "/public/images/1.png",
     tags: ["React", "Node.js", "EmailJS"],
     source: "https://github.com/cortinabryan/Taz-Bulldogs",
     visit: "https://taz-bulldogs.netlify.app/",
@@ -23,7 +23,7 @@ export const projects = [
     title: "The Gentle Art of Brazilian Jiu Jitsu",
     description:
       "My first front-end project. One of my other passions is Brazilian Jiu-Jitsu. My inspiration to create this project is if i were to have my own Jiu-Jitsu school and i would like to have a website/brand similar to this one. I learned primarily how to use CSS Grid layout creating this project",
-    image: "/images/2.png",
+    image: "/public/images/2.png",
     tags: ["HTML", "CSS", "Grid"],
     source: "https://github.com/cortinabryan/BJJ-Website",
     visit: "https://jiujitsu.netlify.app/",
@@ -33,7 +33,7 @@ export const projects = [
     title: "Fight Gear",
     description:
       "I was inspired to create an online martial-arts clothing store. I primarily learned how to use Flexbox. Not a Mobile responsive website",
-    image: "/images/3.png",
+    image: "/public/images/3.png",
     tags: ["HTML", "CSS", "Flexbox"],
     source: "https://github.com/cortinabryan/Fight-Gear-Website",
     visit: "https://fight-gear.netlify.app/",
@@ -43,7 +43,7 @@ export const projects = [
     title: "Flooble",
     description:
       "I wanted to create a google mockup. This was to sharpen my Grid and Flex-box skills. Also turned navbar into a hamburger icon when viewed in mobile",
-    image: "/images/4.png",
+    image: "/public/images/4.png",
     tags: ["HTML", "CSS", "Grid", "Flexbox"],
     source: "https://github.com/cortinabryan/flooble",
     visit: "https://flooble.netlify.app/",
